f73e8c9acb0f5ba6d558315345e0d29762d75bb1
Craig Duncan <git@duncanc.co.uk>
2015-04-18 18:27:48 +0100

    Add a hasClass() method to HtmlElement

--- src/HtmlElement.php
+++ src/HtmlElement.php
@@ -8,4 +8,22 @@ namespace duncan3dc\DomParser;
 class HtmlElement extends HtmlBase
 {
     use Element;
+
+    /**
+     * Check if this element has the specified class applied to it.
+     *
+     * @param string $className The name of the class to check for (case-sensitive)
+     *
+     * @return bool
+     */
+    public function hasClass($className)
+    {
+        if (!$class = $this->dom->getAttribute("class")) {
+            return false;
+        }
+
+        $classes = explode(" ", $class);
+
+        return in_array($className, $classes, true);
+    }
 }
