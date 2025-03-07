4d580cde967bc29a5520f80f51f98f8a6275bd34
Craig Duncan <craig.duncan@estrato.co.uk>
2025-03-07 17:35:28 +0000

    Update the coding standards to the latest version

--- src/Html/AbstractBase.php
+++ src/Html/AbstractBase.php
@@ -96,7 +96,6 @@ abstract class AbstractBase extends \duncan3dc\Dom\AbstractBase
         $url = "";
 
         foreach ($this->childNodes as $node) {
-
             # Recurse
             if (!in_array($node->nodeName, ["input", "select", "textarea"], true)) {
                 $url .= $node->generateUrlFromFormElements();
